ea8eac9a9823bad465235ca1300d4316a4f2e685
yash <yashking0943@gmail.com>
2024-09-28 09:30:11 +0530

    artimatic and relational operators

---
+++ index.js
@@ -0,0 +1,60 @@
+
+
+
+name="yash";
+let salary =1000;
+let age=28;
+let grade='B';
+let address="delhi";
+
+
+console.log(name);
+console.log(age);
+console.log("salary");
+console.log(grade);
+console.log(address);
+
+
+
+console.log("my name is",name);
+
+console.log(`my name is ${name} and my age is ${age}, my salary is ${ salary},and my grade was ${grade},i lived in ${address}`);
+
+process.stdout.write(name)/*
+
+
+/*Aeritimatic operator*/
+console.log(10+20);
+console.log(10-20);
+console.log(10/20);
+console.log(10*10);
+console.log(10%10);
+ 
+/* floor function cell function          */
+ console.log(Math.floor( 11/5));
+ console.log(Math.ceil(11/5));
+    /* relattional opertaot */
+  console.log(10<20);
+  console.log(20>30);
+  console.log(20<=30);
+   console.log(20==30);
+   console.log(20!=30);
+
+/* logical operator*/
+
+   
+     
+  
+  
+   
+ 
+
+
+
+
+
+
+
+
+
+
